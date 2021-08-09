9c354bc9d71e802f7c55e1c0582ffadd1da4f06c
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-08-09 11:15:02 +0200

    wip new attempt beginning

--- view.php
+++ view.php
@@ -22,56 +22,22 @@
 				<?= $result['message'] ?>
 			</div>
 		<form method="post">
+			<?php /*
 			<div class="form-row">
 				<div class="form-group col-md-6">
 					<label for="name">NAME:</label>
 					<input type="name" id="name" name="name" class="form-control" value="<?php echo isset($_SESSION['name']) ? $_SESSION['name'] : '' ?>" />
 				</div>
 			</div>
+			*/ ?>
 
 			<fieldset>
 				<div class="form-row">
 					<div class="form-group col-md-3">
-						<label for="pan">PAN:</label>
-						<input type="text" name="pan" id="pan" class="form-control" value="<?= isset($_SESSION['pan']) ? $_SESSION['pan'] : '' ?>" />
+						<label for="word">(word)</label>
+						<input type="text" name="word" id="word" class="form-control" />
 					</div>
-					<div class="form-group col-md-3">
-						<label for="gnomo">GNOMO DE JARDIN:</label>
-						<input type="text" id="gnomo" name="gnomo" class="form-control" value="<?= isset($_SESSION['gnomo']) ? $_SESSION['gnomo'] : '' ?>" />
-					</div>
-				</div>
-
-				<div class="form-row">
-					<div class="form-group col-md-3">
-						<label for="huevo">HUEVO:</label>
-						<input type="text" id="huevo" name="huevo" class="form-control" value="<?= isset($_SESSION['huevo']) ? $_SESSION['huevo'] : '' ?>" />
-					</div>
-					<div class="form-group col-md-3">
-						<label for="arbusto">ARBUSTO:</label>
-						<input type="text" id="arbusto" name="arbusto" class="form-control" value="<?= isset($_SESSION['arbusto']) ? $_SESSION['arbusto'] : '' ?>" />
-					</div>
-				</div>
-
-				<div class="form-row">
-					<div class="form-group col-md-3">
-						<label for="sombrero">SOMBRERO:</label>
-						<input type="text" id="sombrero" name="sombrero" class="form-control" value="<?= isset($_SESSION['sombrero']) ? $_SESSION['sombrero'] : '' ?>" />
-					</div>
-					<div class="form-group col-md-3">
-						<label for="puerta">PUERTA:</label>
-						<input type="text" id="puerta" name="puerta" class="form-control" value="<?= isset($_SESSION['puerta']) ? $_SESSION['puerta'] : '' ?>" />
-					</div>
-				</div>
-
-				<div class="form-row">
-					<div class="form-group col-md-3">
-						<label for="música">MÚSICA:</label>
-						<input type="text" id="música" name="música" class="form-control" value="<?= isset($_SESSION['música']) ? $_SESSION['música'] : '' ?>" />
-					</div>
-					<div class="form-group col-md-3">
-						<label for="galleta">GALLETA:</label>
-						<input type="text" id="galleta" name="galleta" class="form-control" value="<?= isset($_SESSION['galleta']) ? $_SESSION['galleta'] : '' ?>" />
-					</div>
+					
 				</div>
 
 			</fieldset>
